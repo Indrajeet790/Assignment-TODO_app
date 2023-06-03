82b481fdb0e76e184f22ae50485cda95fca4b767
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-03 22:07:08 +0530

    upatde route for upadting task

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { addTask, getTask } = require("../controller/taskController");
+const {
+  addTask,
+  getTask,
+  updateTask,
+} = require("../controller/taskController");
 
 // route for addTask
 router.post("/", addTask);
 
 router.get("/:id", getTask);
+
+router.put("/:id", updateTask);
 
 module.exports = router;
